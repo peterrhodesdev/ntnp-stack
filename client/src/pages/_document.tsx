73458e16ed5a18510aa8e692f42ec5494cef07f8
peterrhodesdev <peter.rhodes@gmail.com>
2022-11-10 15:07:22 +0700

    create client

---
+++ client/src/pages/_document.tsx
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body className="p-8 prose prose-sm prose-slate max-w-none">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <>
+      <h1>NTNP Stack</h1>
+      <p>Next.js, Tailwind CSS, Nest.js, and PostgreSQL.</p>
+    </>
+  );
+}

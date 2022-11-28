3d2f370fa94ff3c439361d4a5baec8e4a24a47d5
peterrhodesdev <peter.rhodes@gmail.com>
2022-11-28 22:05:39 +0700

    add interceptor tests

--- server/src/interceptors/global-internal-error.interceptor.spec.ts
+++ server/src/interceptors/global-internal-error.interceptor.spec.ts
@@ -1,6 +1,13 @@
 import { createMock } from "@golevelup/ts-jest";
-import { CallHandler } from "@nestjs/common";
-import { of } from "rxjs";
+import {
+  CallHandler,
+  HttpException,
+  InternalServerErrorException,
+  NotFoundException,
+} from "@nestjs/common";
+import { of, throwError } from "rxjs";
+import { CustomException } from "../exceptions/custom.exception";
+import { IdNotFoundException } from "../exceptions/id-not-found.exception";
 import { GlobalInternalErrorInterceptor } from "./global-internal-error.interceptor";
 
 describe("InternalErrorInterceptor", () => {
@@ -28,29 +35,14 @@ describe("InternalErrorInterceptor", () => {
     expect.assertions(1);
   });
 
-  /*test("no error", (done) => {
-    const data = null;
+  test("http exception", (done) => {
+    const error = new HttpException("response", 404);
     const executionContext: any = {};
-    mockCallHandler.handle.mockImplementation(() => of(data));
+    mockCallHandler.handle.mockImplementation(() => throwError(() => error));
 
     interceptor.intercept(executionContext, mockCallHandler).subscribe({
       error: (err) => {
-        expect(err).toBeInstanceOf(NotFoundException);
-        done();
-      },
-    });
-
-    expect.assertions(1);
-  });*/
-
-  /*test("data is undefined", (done) => {
-    const data = undefined;
-    const executionContext: any = {};
-    mockCallHandler.handle.mockImplementation(() => of(data));
-
-    interceptor.intercept(executionContext, mockCallHandler).subscribe({
-      error: (err) => {
-        expect(err).toBeInstanceOf(NotFoundException);
+        expect(err).toEqual(error);
         done();
       },
     });
@@ -58,18 +50,51 @@ describe("InternalErrorInterceptor", () => {
     expect.assertions(1);
   });
 
-  test("valid data", (done) => {
-    const data = { test: "data" };
+  describe("custom exception", () => {
+    test("id not found", (done) => {
+      const error = new IdNotFoundException("id");
+      const executionContext: any = {};
+      mockCallHandler.handle.mockImplementation(() => throwError(() => error));
+
+      interceptor.intercept(executionContext, mockCallHandler).subscribe({
+        error: (err) => {
+          expect(err).toBeInstanceOf(NotFoundException);
+          done();
+        },
+      });
+
+      expect.assertions(1);
+    });
+
+    test("default", (done) => {
+      const error = new CustomException();
+      const executionContext: any = {};
+      mockCallHandler.handle.mockImplementation(() => throwError(() => error));
+
+      interceptor.intercept(executionContext, mockCallHandler).subscribe({
+        error: (err) => {
+          expect(err).toBeInstanceOf(InternalServerErrorException);
+          done();
+        },
+      });
+
+      expect.assertions(1);
+    });
+  });
+
+  test("default", (done) => {
+    class TestDefaultError extends Error {}
+    const error = new TestDefaultError();
     const executionContext: any = {};
-    mockCallHandler.handle.mockImplementation(() => of(data));
+    mockCallHandler.handle.mockImplementation(() => throwError(() => error));
 
     interceptor.intercept(executionContext, mockCallHandler).subscribe({
-      next: (value) => {
-        expect(value).toEqual(data);
+      error: (err) => {
+        expect(err).toBeInstanceOf(InternalServerErrorException);
         done();
       },
     });
 
     expect.assertions(1);
-  });*/
+  });
 });
